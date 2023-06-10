1202c4aef20155bbda80d27636bf810b0a2fd541
quangvinh <quangvinh11602@gmail.com>
2023-06-10 21:12:34 +0700

    vinh-10June

---
+++ YogaCenter/frontend-react/my-app/src/data/ListOfCourses.js
@@ -0,0 +1,45 @@
+export const courses = [
+  {
+    id: 1,
+    type:"Dynamic Yoga",
+    name: "Yoga Vinyasa",
+    inf:"A dynamic form of yoga that combines smooth movements and breath control",
+    img: "/assets/images/class1.jpg",
+  },
+  {
+    id: 2,
+    type:"Gentle and Relaxing Yoga",
+    name: "Hatha Yoga",
+    inf:"Balancing body and mind through basic postures and breath control",
+    img: "/assets/images/class1.jpg",
+  },
+
+  {
+    id: 3,
+    type:"Gentle and Relaxing Yoga",
+    name: "Yin Yoga",
+    inf:" Enhancing flexibility and relaxation through long-held stretches",
+    img: "/assets/images/class1.jpg",
+  },
+  {
+    id: 4,
+    type:"Dynamic Yoga",
+    name: "Power Yoga",
+    inf:"Building strength, endurance, and flexibility through dynamic movements",
+    img: "/assets/images/class1.jpg",
+  },
+  {
+    id: 5,
+    type:"Gentle and Relaxing Yoga",
+    name: 'Restorative Yoga',
+   inf:"Promoting healing and rejuvenation through gentle postures and props",
+    img:"/assets/images/class1.jpg",
+  },
+        {
+    id: 6,
+    type:"Specialized Yoga",
+    name: 'Kundalini Yoga',
+    inf:"Awakening inner energy for heightened awareness and consciousness.",
+    img:"/assets/images/class1.jpg",
+       },
+];
